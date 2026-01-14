0d1c36da3d95f0cd75b2120f3e777049f7bd5832
yozhgoor <yozhgoor@outlook.com>
2026-01-14 17:33:56 +0100

    Get or create configuration file

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+use crate::dotfiles::Dotfiles;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(crate) struct Config {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub remote_url: Option<String>,
+    #[serde(default, skip_serializing_if = "Dotfiles::is_empty")]
+    pub files: Dotfiles,
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            remote_url: None,
+            files: Dotfiles::new(),
+        }
+    }
+
+    pub fn get_or_create() -> Result<Self> {
+        let config_dir = xdg::BaseDirectories::with_prefix(env!("CARGO_PKG_NAME"));
+        let config_file_path = config_dir.place_config_file("config.toml")?;
+
+        let config: Self = match fs::read_to_string(&config_file_path) {
+            Ok(file) => toml::de::from_str(&file)?,
+            Err(_) => {
+                let config = Self::new();
+                fs::write(&config_file_path, toml::ser::to_string(&config)?)?;
+                log::info!("Config file created at: {}", config_file_path.display());
+
+                config
+            }
+        };
+
+        Ok(config)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::io::Write;
+
+mod config;
+mod dotfiles;
+
+use config::Config;
+
+fn main() -> Result<()> {
+    env_logger::builder()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "[{} {}] {}",
+                record.level(),
+                env!("CARGO_PKG_NAME"),
+                record.args()
+            )
+        })
+        .filter(
+            Some(env!("CARGO_PKG_NAME").replace("-", "_").as_ref()),
+            log::LevelFilter::Info,
+        )
+        .init();
+
+    let _config = Config::get_or_create()?;
+
+    Ok(())
 }
