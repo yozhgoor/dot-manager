1b65cbd6ab22a7800d988fe61e3504604c17ca9b
yozhgoor <yozhgoor@outlook.com>
2026-01-15 17:29:24 +0100

    Add the `cli` and `manager` modules

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use anyhow::Result;
 use std::io::Write;
+use clap::Parser;
 
+mod cli;
 mod config;
 mod dotfiles;
+mod manager;
 
 use config::Config;
+use cli::Cli;
 
 fn main() -> Result<()> {
     env_logger::builder()
@@ -23,7 +27,9 @@ fn main() -> Result<()> {
         )
         .init();
 
-    let _config = Config::get_or_create()?;
+    let cli = Cli::parse();
+    let config = Config::get_or_create()?;
+    manager::run(cli, config)?;
 
     Ok(())
 }
---
+++ src/manager.rs
@@ -0,0 +1,7 @@
+use anyhow::Result;
+
+use crate::{cli::Cli, config::Config};
+
+pub fn run(_cli: Cli, _config: Config) -> Result<()> {
+    todo!()
+}
