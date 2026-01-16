5a483e37bf3711c298a7049fe8fb8cc5c8a3c303
yozhgoor <yozhgoor@outlook.com>
2026-01-16 13:16:52 +0100

    Clean up

--- src/manager.rs
+++ src/manager.rs
@@ -2,7 +2,11 @@ use anyhow::{Result, bail};
 use std::fs;
 use std::path::PathBuf;
 
-use crate::{cli::{UpdateMode, Cli}, config::Config, dotfiles::Dotfile};
+use crate::{
+    cli::{Cli, UpdateMode},
+    config::Config,
+    dotfiles::Dotfile,
+};
 
 #[derive(Debug)]
 pub struct Manager(Vec<Status>);
@@ -19,7 +23,11 @@ impl Manager {
     pub fn check(config: Config, log: bool) -> Result<Self> {
         let mut statuses = Self::new();
 
-        for Dotfile { local_path, remote_path } in config.files {
+        for Dotfile {
+            local_path,
+            remote_path,
+        } in config.files
+        {
             let remote_file_path = if let Some(ref remote_root_path) = config.remote_path {
                 remote_root_path.join(&remote_path)
             } else {
@@ -40,14 +48,14 @@ impl Manager {
                         log::warn!("`{}` can be uploaded on remote", local_path.display());
                     }
                     let local_content = fs::read_to_string(&local_path)?;
-                    statuses.push(Status::ToUpload(remote_file_path, local_content));
+                    statuses.push(Status::Upload(remote_file_path, local_content));
                 }
                 (false, true) => {
                     if log {
                         log::warn!("`{}` can be downloaded from remote", local_path.display());
                     }
                     let remote_content = fs::read_to_string(&remote_file_path)?;
-                    statuses.push(Status::ToDownload(local_path, remote_content));
+                    statuses.push(Status::Download(local_path, remote_content));
                 }
                 (true, true) => {
                     let local_content = fs::read_to_string(&local_path)?;
@@ -61,7 +69,7 @@ impl Manager {
                         if log {
                             log::warn!("`{}` can be updated", local_path.display());
                         }
-                        statuses.push(Status::ToUpdate(
+                        statuses.push(Status::Update(
                             (local_path, local_content),
                             (remote_file_path, remote_content),
                         ));
@@ -76,16 +84,18 @@ impl Manager {
     pub fn run(&self, cli: Cli) -> Result<()> {
         for status in &self.0 {
             match status {
-                Status::ToUpdate((local_path, local_content), (remote_path, remote_content)) if cli.update.is_some() => {
+                Status::Update((local_path, local_content), (remote_path, remote_content))
+                    if cli.update.is_some() =>
+                {
                     match cli.update.expect("update is some") {
                         UpdateMode::Local => todo!(),
                         UpdateMode::Remote => todo!(),
                     }
                 }
-                Status::ToUpload(local_path, local_content) if cli.upload => {
+                Status::Upload(local_path, local_content) if cli.upload => {
                     todo!()
                 }
-                Status::ToDownload(remote_path, remote_content) if cli.download => {
+                Status::Download(remote_path, remote_content) if cli.download => {
                     todo!()
                 }
                 _ => {}
@@ -98,7 +108,7 @@ impl Manager {
 
 #[derive(Debug)]
 enum Status {
-    ToUpdate((PathBuf, String), (PathBuf, String)),
-    ToUpload(PathBuf, String),
-    ToDownload(PathBuf, String),
+    Update((PathBuf, String), (PathBuf, String)),
+    Upload(PathBuf, String),
+    Download(PathBuf, String),
 }
